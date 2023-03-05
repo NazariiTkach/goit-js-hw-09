12bd00d9b5a6929d901ef22c621027029e66124a
Nazarii Tkach <113298067+NazariiTkach@users.noreply.github.com>
2023-03-05 12:19:58 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,7 +3,7 @@ const delay = document.querySelector('input[name="delay"]');
 const step = document.querySelector('input[name="step"]');
 const amount = document.querySelector('input[name="amount"]');
 const btnCreatePromise = document.querySelector('button[type="submit"]');
-
+btnCreatePromise.disabled = false;
 
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
